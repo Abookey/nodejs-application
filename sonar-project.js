989e7e44b18885ddee4f959abaeeae0e0bb918e5
Abookey <134658739+Abookey@users.noreply.github.com>
2023-06-29 08:17:55 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://44.201.194.220:9000/',
+  serverUrl: 'http://54.236.201.84:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
